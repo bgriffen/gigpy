cb0dd8cf7b80885d427ab5b647b576b8957fa19d
Brendan Griffen <brendan.f.griffen@gmail.com>
2023-03-22 12:03:37 +1000

    updated for 2023 use

---
+++ scrapebands.py
@@ -0,0 +1,87 @@
+from urllib.request import urlopen
+from bs4 import BeautifulSoup
+import pandas as pd
+import re
+import requests
+from datetime import datetime
+from datetime import datetime,timedelta
+
+def generate_urls_for_next_nweeks(num_weeks):
+    current_date = datetime.now()
+    nthblock = 0
+    lists  = []
+    while nthblock < num_weeks:
+        current_block = current_date.date().isoformat()
+        Updated_date = current_date+ timedelta(weeks=1)
+        later = Updated_date.date().isoformat()
+        #return Updated_date.date().isoformat()
+        listi = ["https://www.bandsintown.com/?date=",current_block,"T14%3A00%3A00%2C",later,"T23%3A59%3A59&date_filter=This+Month"]
+        lists.append("".join(listi))
+        current_date = Updated_date
+        nthblock +=1
+
+    return lists
+
+MATCH_ALL = r'.*'
+def like(string):
+    """
+    Return a compiled regular expression that matches the given
+    string with any prefix and postfix, e.g. if string = "hello",
+    the returned regex matches r".*hello.*"
+    """
+    string_ = string
+    if not isinstance(string_, str):
+        string_ = str(string_)
+    regex = MATCH_ALL + re.escape(string_) + MATCH_ALL
+    return re.compile(regex, flags=re.DOTALL)
+
+
+def find_by_text(soup, text, tag, **kwargs):
+    """
+    Find the tag in soup that matches all provided kwargs, and contains the
+    text.
+
+    If no match is found, return None.
+    If more than one match is found, raise ValueError.
+    """
+    elements = soup.find_all(tag, **kwargs)
+    matches = []
+    for element in elements:
+        if element.find(text=like(text)):
+            matches.append(element)
+    return matches
+    #if len(matches) > 1:
+    #    raise ValueError("Too many matches:\n" + "\n".join(matches))
+    #elif len(matches) == 0:
+    #    return None
+    #else:
+    #    return matches[0]
+
+def parse_band(band):
+    search_item = band.find_all("div")[2].find_all("div")
+    info = {}
+    info['band_name'] = search_item[1].text
+    info['venue'] = search_item[2].text
+    info['dtime'] = search_item[3].text
+    info['num_interested'] = search_item[5].text
+    return info
+
+def get_bands(num_weeks):
+    urls = generate_urls_for_next_nweeks(num_weeks=num_weeks)
+
+    header = {
+      "User-Agent": "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/50.0.2661.75 Safari/537.36",
+      "X-Requested-With": "XMLHttpRequest"
+    }
+    dfis = []
+    for urli in urls:
+        r = requests.get(urli, headers=header)
+        soup = BeautifulSoup(r.text,features="lxml")
+        bands = find_by_text(soup," PM","a")
+        bandinfo = []
+        for band in bands:
+            bandinfo.append(parse_band(band))
+        dfi = pd.DataFrame(bandinfo)
+        dfis.append(dfi)
+
+    return pd.concat(dfis,ignore_index=True).drop_duplicates(['band_name'])
---
+++ spotifybrisbane.py
@@ -0,0 +1,66 @@
+import requests
+import bs4
+#import spotify
+import sys
+import threading
+import numpy as np
+import scrapebands
+
+
+"""
+Contact: Brendan Griffen brendan.f.griffen@gmail.com @brendangriffen
+
+Code will scrape bands playing in next few months in Boston area.
+
+Requirements:
+
+spotipy:        https://spotipy.readthedocs.io/en/2.22.1/?highlight=playlist#
+developer key:  https://devaccount.spotify.com/my-account/keys/
+
+Enjoy the tunes!
+
+"""
+
+import spotipy
+from spotipy.oauth2 import SpotifyOAuth
+import os
+
+scope = "playlist-modify-public"
+
+sp = spotipy.Spotify(auth_manager=SpotifyOAuth(client_id=os.environ['SPOTIPY_CLIENT_ID'],
+                                                client_secret=os.environ['SPOTIPY_CLIENT_SECRET'],
+                                                redirect_uri=os.environ['SPOTIPY_REDIRECT_URI'],scope=scope))
+
+# get bands in the coming 3 months weeks
+dfbands = scrapebands.get_bands(num_weeks=12)
+all_bands = list(set(dfbands['band_name'].str.title()))
+
+print("Here is the dataframe of bands to be added...")
+print(dfbands)
+
+print("Clearing current tracks...")
+current_tracks = sp.user_playlist(os.environ['SPOTIFY_CLIENT_ID'], playlist_id=os.environ['SPOTIFY_CLIENT_PLAYLISTID'])['tracks']
+remove_tracks = [item['track']['uri'] for item in current_tracks['items']]
+if remove_tracks:
+    sp.user_playlist_remove_all_occurrences_of_tracks(os.environ['SPOTIFY_CLIENT_ID'],
+                                                  playlist_id=os.environ['SPOTIFY_CLIENT_PLAYLISTID'],
+                                                  tracks=remove_tracks)
+
+for band in all_bands:
+    results = sp.search(q='artist:' + band, type='artist')
+    artist = results['artists']['items']
+
+    # if there are artists proceed...
+    if len(artist) > 0:
+        lz_uri = artist[0]['uri']
+
+    print("Adding",band)
+    # just get top 2 tracks if available
+    top_tracks = sp.artist_top_tracks(lz_uri)['tracks'][:2]
+    add_tracks = [track['uri'] for track in top_tracks]
+    # add those to playlist
+    if len(add_tracks) == 0:
+            print("> Skipping - no tracks!")
+            continue
+
+    sp.user_playlist_add_tracks(os.environ['SPOTIFY_CLIENT_ID'], playlist_id=os.environ['SPOTIFY_CLIENT_PLAYLISTID'], tracks=add_tracks)
